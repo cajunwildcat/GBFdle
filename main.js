28ceaac8fb245b165209ea254e5022cc5d0b5cae
Eli Cheramie <elicheramie89@gmail.com>
2025-03-17 20:09:04 -0400

    changed randomization methods and shows target after losing or winning at top

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 let searchInput, dropdown, optionsList, guessInput, filteredOptions, activeIndex = -1;
 let guesses = 0;
 const characters = {};
-let target;
+let gameover = false;
 window.onload = e => {
     characterData.forEach(c => {
-        characters[c.name] = c;
+        characters[c.name.replace("&#039;", "'")] = c;
     });
-    const characterNames = characterData.map(c => c.name);
+    const characterNames = Object.keys(characters);
+
+    let target;
 
     dropdown = document.getElementById('dropdown');
     searchInput = document.getElementById('searchInput');
@@ -56,13 +58,15 @@ window.onload = e => {
         }
     }
 
-    //take the date and generate a random index from characterData
+    // Generate a random index based on the current date
     const date = new Date();
-    const index = date.getDate() % characterData.length;
-    target = characterData[index];
-    
+    const seed = date.getFullYear() * 10000 + (date.getMonth() + 1) * 100 + date.getDate(); // YYYYMMDD format
+    const randomIndex = Math.abs(Math.sin(seed) * 10000) % characterData.length; // Deterministic random value
+    target = characterData[Math.floor(randomIndex)];
+
+    const randomizedNames = shuffleArray(characterNames.slice(0));
     document.querySelector("#randomize-button").onclick = e => {
-        document.querySelector('#guess-results').children[0].innerHTML = `<tr>
+        document.querySelector('#guess-results').children[0].innerHTML = `<tr><td><img src="https://gbf.wiki/thumb.php?f=Unknown_square.jpg&w=75"></td></tr><tr>
             <td>Character</td>
             <td>Element</td>
             <td>Race</td>
@@ -79,80 +83,100 @@ window.onload = e => {
         searchInput.disabled = false;
         searchInput.value = '';
         optionsList.innerHTML = '';
-        target = characterData[Math.floor(Math.random() * characterData.length)];
-        document.querySelector("#title").textContent = "Random: " + Math.floor(Math.random() * (999) + 1);
+        let num = Math.floor(Math.random() * characterData.length);
+        target = characters[randomizedNames[num]];
+        document.querySelector("#title").textContent = "Random: " + num;
     }
-}
 
-function renderOptions(options) {
-    optionsList.innerHTML = '';
-    options.forEach((option, index) => {
-        const li = document.createElement('li');
-        li.textContent = option;
-        li.setAttribute('data-index', index);
-        li.addEventListener('click', () => {
-            guess(option);
+
+    function renderOptions(options) {
+        optionsList.innerHTML = '';
+        options.forEach((option, index) => {
+            const li = document.createElement('li');
+            li.textContent = option;
+            li.setAttribute('data-index', index);
+            li.addEventListener('click', () => {
+                guess(option);
+            });
+            optionsList.appendChild(li);
         });
-        optionsList.appendChild(li);
-    });
+    }
 
     activeIndex = options.length > 0 ? 0 : -1; // Set the first option as active, or reset if no options
     updateActiveOption(); // Highlight the active option
-}
 
-function updateActiveOption() {
-    const items = optionsList.querySelectorAll('li');
-    items.forEach((item, index) => {
-        item.classList.toggle('active', index === activeIndex);
-    });
-}
-
-function guess(guess) {
-    guess = characters[guess];
-    console.log(guess);
-    guesses++;
-    searchInput.value = '';
-    optionsList.innerHTML = '';
-
-    const guessRow = document.querySelector('#guess-results').children[0].children[guesses];
-    guessRow.innerHTML = `
-        <td><img src="https://gbf.wiki/thumb.php?f=Npc_s_${guess.id}_01.jpg&w=75"></td>
-        <td ${compareGuess(guess, "element")}><img src="https://gbf.wiki/thumb.php?f=Label_Element_${guess.element}.png&w=70"></td>
-        <td ${compareGuess(guess, "race")}><img src="https://gbf.wiki/thumb.php?f=Label_Race_${guess.race}.png&w=120"></td>
-        <td ${compareGuess(guess, "type")}><img src="https://gbf.wiki/thumb.php?f=Label_Type_${guess.type}.png&w=120"></td>
-        <td ${compareGuess(guess, "weapon", 0)}><img src="https://gbf.wiki/thumb.php?f=Label_Weapon_${guess.weapon[0]}.png&w=80"></td>
-        <td ${compareGuess(guess, "weapon", 1)}>${guess.weapon[1]? `<img src="https://gbf.wiki/thumb.php?f=Label_Weapon_${guess.weapon[1]}.png&w=80">` : ""}</td>
-`
-    if (guess == target) {
-        alert(`You won in ${guesses} guesses!`);
-        searchInput.disabled = true;
+    function updateActiveOption() {
+        const items = optionsList.querySelectorAll('li');
+        items.forEach((item, index) => {
+            item.classList.toggle('active', index === activeIndex);
+        });
     }
-    else if (guesses == 5) {
-        alert(`You lost! The correct character was ${target.name}`);
-        searchInput.disabled = true;
-    }
-}
 
-function compareGuess(guess, field, extra = null) {
-    let correct = guess[field] === target[field]? "green" : "red";
-    if (field == "weapon") {
-        if (guess[field][extra] == target[field][extra]) {
-            correct = "green"
-        } else if (guess[field][extra] == target[field][1-extra]) {
-            correct = "yellow"
+    function guess(userGuess, reveal = false) {
+        userGuess = characters[userGuess];
+        guesses++;
+        searchInput.value = '';
+        optionsList.innerHTML = '';
+
+        let guessRow;
+        if (reveal) {
+            guessRow = document.querySelector('#guess-results').children[0].children[0];
         } else {
-            correct = "red"
+            guessRow = document.querySelector('#guess-results').children[0].children[guesses + 1];
+        }
+        guessRow.innerHTML = `
+            <td><img src="https://gbf.wiki/thumb.php?f=Npc_s_${userGuess.id}_01.jpg&w=75"></td>
+            <td ${compareGuess(userGuess, "element")}><img src="https://gbf.wiki/thumb.php?f=Label_Element_${userGuess.element}.png&w=70"></td>
+            <td ${compareGuess(userGuess, "race")}><img src="https://gbf.wiki/thumb.php?f=Label_Race_${userGuess.race}.png&w=120"></td>
+            <td ${compareGuess(userGuess, "type")}><img src="https://gbf.wiki/thumb.php?f=Label_Type_${userGuess.type}.png&w=120"></td>
+            <td ${compareGuess(userGuess, "weapon", 0)}><img src="https://gbf.wiki/thumb.php?f=Label_Weapon_${userGuess.weapon[0]}.png&w=80"></td>
+            <td ${compareGuess(userGuess, "weapon", 1)}>${userGuess.weapon[1] ? `<img src="https://gbf.wiki/thumb.php?f=Label_Weapon_${userGuess.weapon[1]}.png&w=80">` : ""}</td>
+    `
+        if (!gameover && (userGuess == target || guesses == 5)) {
+            gameover = true;
+            searchInput.disabled = true;
+            guess(target.name, true);
         }
     }
-    else if (field == "race") {
-        if (guess[field][0] == target[field][0] && guess[field][1] == target[field][1]) {
-            correct = "green"
-        } else if (guess[field][0] == target[field][0] && guess[field][1] != target[field][1] || 
+
+    function compareGuess(guess, field, extra = null) {
+        let correct = guess[field] === target[field] ? "green" : "red";
+        if (field == "weapon") {
+            if (guess[field][extra] == target[field][extra]) {
+                correct = "green"
+            } else if (guess[field][extra] == target[field][1 - extra]) {
+                correct = "yellow"
+            } else {
+                correct = "red"
+            }
+        }
+        else if (field == "race") {
+            if (guess[field][0] == target[field][0] && guess[field][1] == target[field][1]) {
+                correct = "green"
+            } else if (guess[field][0] == target[field][0] && guess[field][1] != target[field][1] ||
                 (guess[field][1] && target[field][1] && guess[field][0] != target[field][0] && guess[field][1] == target[field][1])) {
-            correct = "yellow"
-        } else {
-            correct = "red"
+                correct = "yellow"
+            } else {
+                correct = "red"
+            }
         }
+        return `style="background-color: ${correct}"`;
     }
-    return `style="background-color: ${correct}"`;
+
+}
+
+//https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffleArray(array) {
+    for (let i = array.length - 1; i >= 0; i--) {
+        const j = Math.floor(random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+//https://stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript
+var seed = 3040130000;
+function random() {
+    var x = Math.sin(seed++) * 10000;
+    return x - Math.floor(x);
 }
